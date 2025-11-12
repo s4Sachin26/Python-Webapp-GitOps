bdd58d24ff7f4c63e3daae6f68c9ecb94b718f96
s4Sachin26 <164172633+s4Sachin26@users.noreply.github.com>
2025-11-12 13:40:08 -0600

    Add comment for container networking requirement
    Added a comment to indicate the necessity of the app.run method for container networking.

--- src/run.py
+++ src/run.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.jinja_env.auto_reload = True
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port) # nosec B104: Required for container networking
+
