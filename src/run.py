4a24c95c263f01d79d8e10e0b9cf6e51ea335cc1
s4Sachin26 <164172633+s4Sachin26@users.noreply.github.com>
2025-11-12 13:45:30 -0600

    Fix formatting issues in run.py

--- src/run.py
+++ src/run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.jinja_env.auto_reload = True
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.run(host="0.0.0.0", port=port) #  nosec B104: Required for container networking
+    app.run(host="0.0.0.0", port=port)  # nosec B104: Required for container networking
